99cacfc031848e931f4c54361f838b6fce8d0c8b
Narias1999 <nicolasarias870@gmail.com>
2018-04-27 10:55:03 -0500

    httpe request and movie component

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,23 @@
+Vue.component('movie', {
+  props: ['imgurl', 'overview', 'title'],
+  template: `<div class="movie">
+  <img :src=imgurl>
+  <div class="mask">
+      <p>{{overview}}...</p>
+      <div class="actions">
+          <h3>{{title}}</h3>
+          <a href="#" class="btn">Ver Más</a>
+      </div>
+  </div>
+</div>`
+})
 var app = new Vue({
     el: '#app',
     data: {
-      movie: null
+      movies: [],
+      movie: null,
+      baseIMG: 'https://image.tmdb.org/t/p/w500',
+      baseTrailer: 'https://www.youtube.com/embed'
     },
     methods: {
       async search () {
@@ -9,13 +25,30 @@ var app = new Vue({
           let search = this.movie.toLowerCase()
           search = search.split(' ').join('+')
           try {
-            let res = await fetch(`https://itunes.apple.com/search?term=${search}&media=movie&entity=movie`)
+            const API_KEY = '5145398a1f237d870d5d1de9074e1220'
+            let res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${API_KEY}&language=es`)
             res = await res.json()
-            console.log(res)
+            if(res.results.length) {
+             this.movies = res.results
+             .filter(el => {
+              return el.backdrop_path && el.overview
+             })
+             .map(el => {
+                el.sumarizedOverview = el.overview.length < 140 ? el.overview : el.overview.slice(0,140)
+                return el
+             })
+             console.log(this.movies)
+             const top = this.$refs.movieSection.offsetTop
+             window.scroll({
+               left:0,
+               top,
+               behavior: 'smooth'
+             })
+            } else alert('no se encontroó ninguna película')
           } catch (error) {
             console.error(error)
           }
         } else alert('debes buscar algo')
-      }
+      },
     }
   })

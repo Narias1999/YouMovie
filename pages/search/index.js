1ce2998dc1e1f1b6a91a14fee658bd67d7ffb50d
Narias1999 <nicolasarias870@gmail.com>
2018-04-26 19:05:55 -0500

    making http request with vue

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,6 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      movie: null
+    },
+    methods: {
+      async search () {
+        if(this.movie) {
+          let search = this.movie.toLowerCase()
+          search = search.split(' ').join('+')
+          try {
+            let res = await fetch(`https://itunes.apple.com/search?term=${search}&media=movie&entity=movie`)
+            res = await res.json()
+            console.log(res)
+          } catch (error) {
+            console.error(error)
+          }
+        } else alert('debes buscar algo')
+      }
     }
   })
